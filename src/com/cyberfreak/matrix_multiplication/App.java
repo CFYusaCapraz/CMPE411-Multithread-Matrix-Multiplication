87d4daf7b2e91807e55d85faec9b34b66d122051
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-06-10 12:40:06 +0300

    Run class

---
+++ src/com/cyberfreak/matrix_multiplication/App.java
@@ -0,0 +1,6 @@
+package com.cyberfreak.matrix_multiplication;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
